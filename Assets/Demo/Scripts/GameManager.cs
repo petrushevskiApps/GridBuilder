39f3d0b2d1a1e6e19826db7f7d0c56971cce755b
Aleksandar Petrushevski <petrushevskiapps@gmail.com>
2021-04-25 01:55:03 +0200

    Initial push

---
+++ Assets/Demo/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Grid;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GridCreator gridCreator;
+
+    public void Awake()
+    {
+        gridCreator.CreateGrid<Tile>(6, 6);
+    }
+}
---
+++ Assets/GridBuilder/Scripts/GridWorldCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Grid
+{
+    public class GridWorldCamera : MonoBehaviour
+    {
+        private Camera gridCamera;
+
+        private void Awake()
+        {
+            GridCreator.GridCreated.AddListener(OnGridCreated);
+            gridCamera = GetComponent<Camera>();
+        }
+
+        private void OnDestroy()
+        {
+            GridCreator.GridCreated.RemoveListener(OnGridCreated);
+        }
+
+        private void OnGridCreated(GridWorldSize worldSize)
+        {
+            if (gridCamera != null)
+            {
+                gridCamera.orthographicSize = worldSize.GetWorldSize().x + 3.5f;
+            }
+        }
+    }
+
+}
+
